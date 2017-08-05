8370cc25fca72753e369153a613b943a4c45acbb
luiscarm9 <lc.rivera10@uniandes.edu.co>
2017-08-05 13:25:53 -0500

    beta code

--- DataStructures/AVL_def/BalancedTree.py
+++ DataStructures/AVL_def/BalancedTree.py
@@ -20,6 +20,22 @@ class Balancedtree(object):
     
     def rebalanceTree(self,parentNode):
         self.setBalanced(parentNode)
+        
+        if parentNode.balanced<-1:
+            if self.height(parentNode.leftChild.leftChild)>=self.height(parentNode.leftChild.rightChild):
+                parentNode=self.rotateRight(parentNode)
+            else:
+                parentNode=self.rotateLeftRight(parentNode)
+        elif parentNode.balanced>1:
+            if self.height(parentNode.rightChild.rightChild)>=self.height(parentNode.rightChild.leftChild):
+                parentNode=self.rotateLeft(parentNode)
+            else:
+                parentNode=self.rotateRightLeft(parentNode)
+        if parentNode.parentNode is not None:
+            self.rebalanceTree(parentNode.parentNode)
+        else:
+            self.rootNode=parentNode
+                
     
     def setBalanced(self,node):
         node.balanced=self.height(node.rightChild-self.height(node.leftChild))
@@ -31,6 +47,71 @@ class Balancedtree(object):
         else:
             return 1+max(self.height(node.leftChild),self.height(node.rightChild))
     
+    # Rotations     
+    
+    def rotateLeftRight(self,node):
+        print 'Left-Right Rotation'
+        node.leftChild=self.rotateLeft(node.leftChild)
+        return self.rotateRight(node)
+    
+    def rotateRightLeft(self,node):
+        print 'Right-Left Rotation'
+        node.rightChild=self.rotateRight(node.rightChild)
+        return self.rotateLeft(node)
+
+    def rotateLeft(self,node):
+        print 'Left Rotation'
+        
+        temp=node.rightChild
+        temp.parenNode=node.parentNode
+        
+        node.rightChild=temp.leftChild
+        
+        if node.rightChild is not None:
+            node.rightChild.parenNode=node
+        
+        temp.leftChild=node
+        node.parentNode=temp
+        
+        if temp.parenNode is not None:
+            if temp.parenNode.rightChild==node:
+                temp.parenNode.rightChild=temp
+            else:
+                temp.parenNode.rightChild=temp
+        
+        self.setBalanced(temp)
+        self.setBalanced(node)
+        
+        return temp
+    
+    
+    def rotateRight(self,node):
+        print 'Right Rotation'
+        
+        temp=node.leftChild
+        temp.parenNode=node.parentNode
+        
+        node.leftChild=temp.rightChild
+        
+        if node.leftChild is not None:
+            node.leftChild.parenNode=node
+        
+        temp.rightChild=node
+        node.parentNode=temp
+        
+        if temp.parenNode is not None:
+            if temp.parenNode.rightChild==node:
+                temp.parenNode.rightChild=temp
+            else:
+                temp.parenNode.leftChild=temp
+        
+        self.setBalanced(temp)
+        self.setBalanced(node)
+        
+        return temp
+        
+    
+
     
         
         
